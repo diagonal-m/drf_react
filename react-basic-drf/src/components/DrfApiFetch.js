66f6943c29ad2f22d8912efaa16b3d5ee607e370
佐藤太一 <3101taichi@gmail.com>
2021-09-17 14:53:12 +0900

    createメソッド実装

--- react-basic-drf/src/components/DrfApiFetch.js
+++ react-basic-drf/src/components/DrfApiFetch.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 const DrfApiFetch = () => {
   const [tasks, setTasks] = useState([])
   const [selectedTask, setSelectedTask] = useState([])
+  const [editedTask, setEditedTask] = useState({id: '', title: ''})
   const [id, setId] = useState(1)
 
   useEffect(() => {
@@ -33,6 +34,25 @@ const DrfApiFetch = () => {
     .then(res => {setTasks(tasks.filter(task => task.id !== id)); setSelectedTask([])})
   }
 
+  const newTask = (task) => {
+    const data = {
+      title: task.title
+    }
+    axios.post(`http://localhost:8000/api/tasks/`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token b9be0cb85a1ab69fda8014aad6c1fa1e6a0f2962'
+      }
+    })
+    .then(res => setTasks([...tasks, res.data]))
+  }
+
+  const handleInputChange = () => evt => {
+    const value = evt.target.value;
+    const name = evt.target.name;
+    setEditedTask({...editedTask, [name]:value})
+  }
+
   return (
     <div>
       <ul>
@@ -52,6 +72,9 @@ const DrfApiFetch = () => {
       <button type='button' onClick={()=>getTask()}>Get task</button>
       {/* <button type='button' onClick={()=>deleteTask()}>Delete</button> */}
       <h3>{selectedTask.title} {selectedTask.id}</h3>
+
+      <input type="text" name="title" value={editedTask.title} onChange={handleInputChange()} placeholder="New task?" required/>
+      <button onClick={()=>newTask(editedTask)}>Create</button>
     </div>
   )
 }
